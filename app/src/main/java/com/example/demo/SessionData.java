90b30da309db7bc38b34dc5d9d15881cd35fbcdb
Stephan Hüwe <45753488+sHuewe@users.noreply.github.com>
2026-08-27 19:20:37 +0200

    Make app session opaque to auth manager

--- app/src/main/java/com/example/demo/SessionData.java
+++ app/src/main/java/com/example/demo/SessionData.java
@@ -1,44 +1,11 @@
 package com.example.demo;
 
-import com.nimbusds.oauth2.sdk.id.State;
-import com.nimbusds.openid.connect.sdk.Nonce;
+import java.util.UUID;
 
 public final class SessionData {
-    private volatile State expectedState;
-    private volatile Nonce expectedNonce;
-    private volatile TokenSet tokens;
+    private final String authId = UUID.randomUUID().toString();
 
-    public State getExpectedState() {
-        return expectedState;
-    }
-
-    public void setExpectedState(State expectedState) {
-        this.expectedState = expectedState;
-    }
-
-    public Nonce getExpectedNonce() {
-        return expectedNonce;
-    }
-
-    public void setExpectedNonce(Nonce expectedNonce) {
-        this.expectedNonce = expectedNonce;
-    }
-
-    public TokenSet getTokens() {
-        return tokens;
-    }
-
-    public void setTokens(TokenSet tokens) {
-        this.tokens = tokens;
-    }
-
-    public void clearLoginAttempt() {
-        this.expectedState = null;
-        this.expectedNonce = null;
-    }
-
-    public void clear() {
-        clearLoginAttempt();
-        this.tokens = null;
+    public String getAuthId() {
+        return authId;
     }
 }
